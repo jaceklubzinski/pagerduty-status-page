28df373e0ba58b9a321c8ce76257a02eb771d2c3
Jacek Lubziński <jacek@piwik.pro>
2021-05-20 21:05:54 +0200

    todo admin on duty

--- pkg/pd/pd.go
+++ pkg/pd/pd.go
@@ -12,6 +12,10 @@ func NewAPIClient(client *pagerduty.Client) *APIClient {
 	return &APIClient{client: client}
 }
 
+/**
+ * @todo Add admin on duty
+ * @body Incidents can be assigned to the person that is not on duty currently, additional info about admin on duty will be appreciated
+ */
 type Lister interface {
 	ListI(opts pagerduty.ListIncidentsOptions) (*pagerduty.ListIncidentsResponse, error)
 	ListS() ([]pagerduty.Service, error)
